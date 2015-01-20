ceb64b6a251d926085b448816fa04e599540086c
Alan Gardner <alan@armoin.com>
2015-01-20 11:54:13 +0000

    Removing unrequired params
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'jade');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('index', {title: 'Hey', message: 'Hello there!'})
+  res.render('index')
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
