0e9bbfbe136533907a9972e3594233157d98272d
Alan Gardner <alan@armoin.com>
2015-01-20 11:22:47 +0000

    Fixing issues on Heroku
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.render('index', {title: 'Hey', message: 'Hello there!'})
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
   var port = server.address().port
 
